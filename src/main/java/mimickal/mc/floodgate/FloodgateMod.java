b2cfe64da3c49b513651236f6af2c580e9ab986c
Mia Moretti <thekillerdonut1@aol.com>
2017-08-18 01:53:19 -0400

    Updated example code with actual project names and stuff

--- src/main/java/mimickal/mc/floodgate/FloodgateMod.java
+++ src/main/java/mimickal/mc/floodgate/FloodgateMod.java
@@ -1,15 +1,21 @@
-package com.example.examplemod;
+package mimickal.mc.floodgate;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(
+        modid = FloodgateMod.MODID,
+        name = FloodgateMod.NAME,
+        version = FloodgateMod.VERSION,
+        acceptedMinecraftVersions = "[1.10.2]"
+)
+public class FloodgateMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "floodgate";
+    public static final String NAME = "Floodgate";
+    public static final String VERSION = "1.10.2-1.0.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
